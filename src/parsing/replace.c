3d68194f305c008da6f75c38772568fe30c7d7ea
Thomas Barret <tbarret@paul-f4Ar8s5.clusters.42paris.fr>
2024-05-11 18:22:06 +0200

    Fix: Norm replace.c

--- src/parsing/replace.c
+++ src/parsing/replace.c
@@ -6,7 +6,7 @@
 /*   By: tbarret <tbarret@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/22 13:30:16 by mkane             #+#    #+#             */
-/*   Updated: 2024/05/11 18:08:58 by tbarret          ###   ########.fr       */
+/*   Updated: 2024/05/11 18:21:43 by tbarret          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -54,15 +54,35 @@ static int	count_quoted_chars(char *str, int start)
 	return (len);
 }
 
+static void	update_tmp(char **tmp, char *str, int *j, t_minishell *minishell)
+{
+	char	*tmp2;
+
+	if (str[*j] == '?')
+	{
+		*tmp = ft_itoa(get_status(0, 3));
+		(*j)++;
+		while (str[*j] && (ft_isalnum(str[*j])
+				|| str[*j] == '_' || str[*j] == '?'))
+		{
+			tmp2 = ft_calloc(2, sizeof(char));
+			tmp2[0] = str[*j];
+			*tmp = ft_strjoin(*tmp, tmp2);
+			free(tmp2);
+			(*j)++;
+		}
+	}
+	else
+		*tmp = find_env(minishell->env, str + *j);
+}
+
 static int	count_env_chars(char *str, t_minishell *minishell, int i)
 {
-	int		len;
 	int		j;
-	char	*tmp;
 	char	c;
 
-	len = 0;
-	tmp = NULL;
+	int (len) = 0;
+	char *(tmp) = NULL;
 	while (str[i] && str[i] != '$')
 		i++;
 	if (str[i] == '$' && after_dollars(str[i + 1]))
@@ -73,20 +93,7 @@ static int	count_env_chars(char *str, t_minishell *minishell, int i)
 			i++;
 		c = str[i];
 		str[i] = '\0';
-		if (str[j] == '?')
-		{
-			tmp = ft_itoa(get_status(0, 3));
-			j++;
-			while (str[j] && (ft_isalnum(str[j]) || str[j] == '_' || str[j] == '?'))
-			{
-				char *tmp2 = ft_calloc(2, sizeof(char));
-				tmp2[0] = str[j];
-				tmp = ft_strjoin(tmp, tmp2);
-				free(tmp2);
-				j++;
-			}
-		} else
-			tmp = find_env(minishell->env, str + j);
+		update_tmp(&tmp, str, &j, minishell);
 		str[i] = c;
 		if (tmp)
 		{
